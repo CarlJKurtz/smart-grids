f578c117d858e11cfdf9d915f14adbdea8290ea4
Carl Kurtz <carl.janik.kurtz@gmail.com>
2023-02-10 19:37:06 +0100

    exceptions for file loading

--- file_handler.py
+++ file_handler.py
@@ -1,47 +1,68 @@
 from config import *
 from xml.dom import minidom
 import xmltodict
+from unitlineedit import UnitLineEdit
+import PyQt5
 
 
 def read_file(window, path):
-    file = open(f'{path}', 'r')
-    content = file.read()
-    dict = xmltodict.parse(content)['root']
+    try:
+        file = open(f'{path}', 'r')
+        content = file.read()
+        dict = xmltodict.parse(content)['root']
 
-    window.page_width_spinbox.setValue(float(dict['page']['@page-width']))
-    window.page_height_spinbox.setValue(float(dict['page']['@page-height']))
-    window.top_margin_spinbox.setValue(float(dict['page']['@top-margin']))
-    window.bottom_margin_spinbox.setValue(float(dict['page']['@bottom-margin']))
-    window.left_margin_spinbox.setValue(float(dict['page']['@left-margin']))
-    window.right_margin_spinbox.setValue(float(dict['page']['@right-margin']))
+        file_version = dict['document-preferences']['@version']
+        if file_version != APP_VERSION:
+            window.throw_error(f'This file was saved in a different version of {APP_TITLE}.\n'
+                               f'An error while loading this file might occur.')
 
-    if dict['font']['@key'] in window.font_dict:
-        window.font_dropdown.setCurrentText((dict['font']['@key']))
+        try:
+            window.dpi = int(dict['document-preferences']['@dpi'])
+            window.change_unit(dict['document-preferences']['@unit'])
+            window.resolution_output.setText(f'{window.dpi} dpi')
+            window.page_width_spinbox.setValue(float(dict['page']['@page-width']))
+            window.page_height_spinbox.setValue(float(dict['page']['@page-height']))
+            window.top_margin_spinbox.setValue(float(dict['page']['@top-margin']))
+            window.bottom_margin_spinbox.setValue(float(dict['page']['@bottom-margin']))
+            window.left_margin_spinbox.setValue(float(dict['page']['@left-margin']))
+            window.right_margin_spinbox.setValue(float(dict['page']['@right-margin']))
 
-    window.font_size_spinbox.setValue(float(dict['font']['@size']))
-    window.leading_spinbox.setValue(float(dict['font']['@leading']))
+            if dict['font']['@key'] in window.font_dict:
+                window.font_dropdown.setCurrentText((dict['font']['@key']))
+            else:
+                font_name = dict['font']['@key']
+                window.throw_error(f'The font {font_name} does not exist on your system.')
 
-    if dict['vertical-alignment']['@upper-alignment'] == 'ascender':
-        window.ascender_radio.setChecked(True)
-    elif dict['vertical-alignment']['@upper-alignment'] == 'cap-height':
-        window.cap_height_radio.setChecked(True)
-    elif dict['vertical-alignment']['@upper-alignment'] == 'x-height':
-        window.xheight_radio.setChecked(True)
+            window.font_size_spinbox.setValue(float(dict['font']['@size']))
+            window.leading_spinbox.setValue(float(dict['font']['@leading']))
 
-    if dict['vertical-alignment']['@lower-alignment'] == 'baseline':
-        window.baseline_radio.setChecked(True)
-    elif dict['vertical-alignment']['@lower-alignment'] == 'descender':
-        window.descender_radio.setChecked(True)
+            if dict['vertical-alignment']['@upper-alignment'] == 'ascender':
+                window.ascender_radio.setChecked(True)
+            elif dict['vertical-alignment']['@upper-alignment'] == 'cap-height':
+                window.cap_height_radio.setChecked(True)
+            elif dict['vertical-alignment']['@upper-alignment'] == 'x-height':
+                window.xheight_radio.setChecked(True)
 
-    window.rows_spinbox.setValue(int(dict['grid']['@rows']))
-    window.lines_in_gutter_spinbox.setValue(int(dict['grid']['@gutterlines']))
-    window.columns_spinbox.setValue(int(dict['grid']['@columns']))
-    window.custom_gutter_spinbox.setValue(float(dict['grid']['@gutter']))
+            if dict['vertical-alignment']['@lower-alignment'] == 'baseline':
+                window.baseline_radio.setChecked(True)
+            elif dict['vertical-alignment']['@lower-alignment'] == 'descender':
+                window.descender_radio.setChecked(True)
 
-    if dict['grid']['@use_custom_gutter'] == 'True':
-        window.use_custom_gutter_checkbox.setChecked(True)
-    else:
-        window.use_custom_gutter_checkbox.setChecked(False)
+            window.rows_spinbox.setValue(int(dict['grid']['@rows']))
+            window.lines_in_gutter_spinbox.setValue(int(dict['grid']['@gutterlines']))
+            window.columns_spinbox.setValue(int(dict['grid']['@columns']))
+            window.custom_gutter_spinbox.setValue(float(dict['grid']['@gutter']))
+
+            if dict['grid']['@use_custom_gutter'] == 'True':
+                window.use_custom_gutter_checkbox.setChecked(True)
+            else:
+                window.use_custom_gutter_checkbox.setChecked(False)
+
+        except:
+            pass
+
+    except:
+        window.throw_error(f'Oops!\n{APP_TITLE} can’t read this file.', icon=PyQt5.QtWidgets.QMessageBox.Critical)
 
     window.update()
 
@@ -58,6 +79,12 @@ def write_xml_content(window):
     root = minidom.Document()
     xml = root.createElement('root')
     root.appendChild(xml)
+
+    preferences_child = root.createElement('document-preferences')
+    preferences_child.setAttribute('version', str(APP_VERSION))
+    preferences_child.setAttribute('unit', str(UnitLineEdit.unit))
+    preferences_child.setAttribute('dpi', str(window.dpi))
+    xml.appendChild(preferences_child)
 
     page_child = root.createElement('page')
     page_child.setAttribute('page-width', str(window.page_width_spinbox.value()))
